3c2b9e2ce275230d2f4536fff947e24822f7c8b4
Matthew Lu <seedmachinegun@gmail.com>
2022-04-02 04:44:19 +0100

    change to stream to avoid the results being to big for memory

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express')
-const zipDirectory = require('./zipDirectory')
+const JSZip = require('jszip')
+const fs = require('fs')
+const { Base64Encode } = require('base64-stream')
+const async = require('async')
 const path = require('path')
+const Archiver = require('archiver')
 
 const app = express()
 const PORT = 8888
@@ -9,12 +13,58 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'))
 })
 app.get('/download', async (req, res) => {
-    // Start zipping
-    const zip = await zipDirectory('./files')
-    const data = await zip.generateAsync({ type: 'base64' })
-    // TODO: use stream instead
-    res.send(data)
+    console.log(process.memoryUsage())
+    res.set('Content-Type','application/zip');
+    res.set('Content-disposition', 'attachment; filename=myFile.zip')
+
+    // Create zip file
+    // const zip = new JSZip()
+    const zip = new Archiver('zip')
+
+    zip
+        .pipe(new Base64Encode())
+        .pipe(res)
+
+    // Go through directory and add files
+    const parentDir = path.join(__dirname, 'files')
+
+    async.waterfall([
+        function (cb) {
+            fs.readdir(parentDir, cb);
+        },
+        function (files, cb) {
+            // Consume 10 files in parallel.
+            async.eachLimit(files, 10, function (filename, done) {
+                const filePath = path.join(parentDir, filename)
+
+                if (fs.statSync(filePath).isFile()) {
+                    const stream = fs.createReadStream(filePath)
+                    // zip.file(filename, stream)
+                    zip.append(stream, { name: filename })
+                }
+                done()
+            }, cb)
+        }
+    ], function (err) {
+        err && console.trace(err)
+        if (!err) {
+            console.log('Done adding files')
+            // Send data to client
+            console.log(process.memoryUsage())
+            zip.finalize()
+            // zip
+            //     .generateNodeStream({type: 'nodebuffer', streamFiles: true})
+            //     .pipe(new Base64Encode())
+            //     .pipe(res)
+            //     .on('finish', function () {
+            //         console.log('Finish sending');
+            //         console.log(process.memoryUsage())
+
+            //     })
+        }
+    })
 })
+
 app.listen(PORT, () => {
     console.log(`Server running at ${PORT}.`)
 })
